592d6f5f9a3ec29a46b02087668ad1220fc227a5
agesa3 <collinsagesa2000@gmail.com>
2021-10-03 22:21:51 +0300

    swl statements for creating a database and tabke

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 const express=require('express')
-const mysqli = require('mysql')
+const mysql = require('mysql')
 
- 
 const app = express()
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'dracula',
+    database : 'nodemysql'
+    
+  });
+
+//connect
+db.connect((err)=>{
+    if(err){
+       throw err
+    }
+    console.log("Mysql Connected Successfully")
+})
+
+app.get('/createDb',(req,res)=>{
+    let sql="CREATE DATABASE nodemysql";
+    db.query(sql,(err,result)=>{
+        if(err) throw err
+        console.log(result)
+        res.send('"Database Created Successfully')
+    })
+})
+
+//create table
+app.get('/createpoststable',(req,res)=>{
+    let sql='CREATE TABLE posts(id int AUTO_INCREMENT,title VARCHAR(255),body VARCHAR(255), PRIMARY KEY (id))'
+    db.query(sql,(err,result)=>{
+        if(err) throw err
+        console.log(result)
+        res.send('Posts Table created')
+
+    })
+})
 
 
 
